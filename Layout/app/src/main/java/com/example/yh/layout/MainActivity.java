40ad33d1a5f56940c97a82dd4f15033e99488063
wyChris <281269704@qq.com>
2018-01-02 17:12:12 +0800

    新建

---
+++ Layout/app/src/main/java/com/example/yh/layout/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.yh.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.layout);
+        setContentView(R.layout.layout2);
+        TextView text1 = (TextView) findViewById(R.id.text1);
+        System.out.print(text1.toString());
+
+    }
+}
