00df17e0a578fb9a5160dcf6dd2a273502bceb87
wyChris <281269704@qq.com>
2018-03-09 10:44:44 +0800

    123123

--- OkHttp/app/src/main/java/com/example/yh/okhttp/MainActivity.java
+++ OkHttp/app/src/main/java/com/example/yh/okhttp/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -14,10 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("1111111","");
-        getDataasync();
-        Log.d("222222","msg");
-
+       // getDataAsync();   //异步
+        getDataasync();   //同步
     }
 
     public void getDataasync() {
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 try{
                     OkHttpClient client = new OkHttpClient();
                     Request request = new  Request.Builder()
-                            .url("http://www.baidu.com")
+                            .url("http://www.k12chn.com/m17/m1717I/m1717I002/schoolId/5068")
                             .build();
                     Response response = null;
                     response = client.newCall(request).execute();
@@ -44,5 +46,29 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }).start();
+    }
+
+    public void getDataAsync() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://www.k12chn.com/m17/m1717I/m1717I002/schoolId/5068")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    Log.d("kwwl","获取成功");
+                    String json =  response.body().string();
+                    Log.d("kwwl", json) ;
+                    Log.d("kwwl",response.message()) ;
+                }
+            }
+
+        });
     }
 }
